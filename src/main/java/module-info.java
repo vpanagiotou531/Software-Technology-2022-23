513f387420ae228d5bccb001d02d8b3a0ea1c2de
IO PAG <95532434+Jaeger8K@users.noreply.github.com>
2023-05-27 12:50:39 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.software_technology {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.software_technology to javafx.fxml;
-    exports com.example.software_technology;
-}
