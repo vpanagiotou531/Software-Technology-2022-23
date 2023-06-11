78376e7c1b167ca0bd73e4ce9e54efe0f99a4e49
odysseas-zachos <odysszaxos@gmail.com>
2023-06-11 21:48:52 +0300

    .

--- Software_Technology/src/main/java/com/example/software_technology/AvailableRoomsController.java
+++ Software_Technology/src/main/java/com/example/software_technology/AvailableRoomsController.java
@@ -1,4 +1,33 @@
 package com.example.software_technology;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
 public class AvailableRoomsController {
+
+    private Stage room_stage,employee_stage;
+    private Stage primary_stage = HotelMain.get_stage();
+    private Connection connection = HotelMain.get_connection();
+
+
+
+
 }
--- Software_Technology/src/main/java/com/example/software_technology/CustomerController2.java
+++ Software_Technology/src/main/java/com/example/software_technology/CustomerController2.java
@@ -80,6 +80,10 @@ public class CustomerController2 implements Initializable {
     }
 
     @FXML
+    private Menu available_rooms;
+
+
+    @FXML
     private ChoiceBox<String> choiceBox;
 
     @FXML
@@ -106,13 +110,60 @@ public class CustomerController2 implements Initializable {
     @FXML
     private DatePicker checkOutDate;
 
+    @FXML
+    protected void change_scene_available_rooms() throws IOException, SQLException {
+
+        Stage primary_stage = HotelMain.get_stage();
+        Connection connection = HotelMain.get_connection();
+
+        HotelMain.change_scene(11);
+        primary_stage.getScene().setRoot(new GridPane());
+
+        GridPane gridPane = (GridPane) primary_stage.getScene().getRoot();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+
+        String sql2 = "SELECT * FROM ROOM WHERE ROOM_TYPE = 'choiceBox1' AND ROOM_STATUS = 'GREEN'";
+        Statement statement = connection.createStatement();
+        ResultSet resultset = statement.executeQuery(sql2);
+
+        int col = 1;
+
+        for (int row = 0; resultset.next(); ) {
+
+            if (col == 1)
+                row++;
+            Rectangle box = new Rectangle(100, 100); // Create a rectangle as a box
+            box.setId(Integer.toString((row - 1) * 4 + col));
+
+            System.out.println(resultset.getString("ROOM_STATUS")+"  " +"RED");
+
+            String name = resultset.getString("ROOM_ID"); // Replace with the actual name you want to display
+            Text nameText = new Text(name);
+            nameText.setId(Integer.toString((row - 1) * 4 + col));
+            nameText.setWrappingWidth(100); // Set the width of the text box
+            nameText.setFont(Font.font("Arial", FontWeight.BOLD, 12));
+            nameText.setTextAlignment(TextAlignment.CENTER);
+            nameText.setMouseTransparent(true);
+
+            box.setStroke(Color.BLACK);
+            box.setStrokeWidth(2);
+
+            gridPane.add(box, col, row + 1); // Add the box to the GridPane
+            gridPane.add(nameText, col, row + 1);
+            col = (col % 4) + 1;
+        }
+    }
+
 
     @Override  // Ορισμός περιεχομένου μέσα στη λίστα τύπων δωματίου
     public void initialize(URL url, ResourceBundle resourceBundle) {
         choiceBox.getItems().addAll("ECONOMIC", "NORMAL", "SUITE");
     }
 
-     @FXML
+    @FXML
     private void button_showButton() throws IOException, SQLException {
 
         String choiceBox1 = choiceBox.getValue();
@@ -132,66 +183,10 @@ public class CustomerController2 implements Initializable {
         String ID = HotelMain.getID();
         System.out.println(ID);
 
-         GridPane gridPane = (GridPane) primary_stage.getScene().getRoot();
-         gridPane.setPadding(new Insets(10));
-         gridPane.setHgap(10);
-         gridPane.setVgap(10);
-         gridPane.setAlignment(Pos.CENTER);
-
-         String sql2 = "SELECT * FROM room WHERE  ";
-         Statement statement = connection.createStatement();
-         ResultSet resultset = statement.executeQuery(sql);
-
-         int col = 1;
-
-         for (int row = 0; resultset.next(); ) {
-
-             if(col==1)
-                 row++;
-             Rectangle box = new Rectangle(100, 100); // Create a rectangle as a box
-             box.setId(Integer.toString((row-1)*4+col));
-
-             System.out.println(resultset.getString("ROOM_STATUS")+"  " +"RED");
-
-             if(resultset.getString("ROOM_STATUS").equalsIgnoreCase("RED")){
-                 box.setFill(Color.RED);
-             }
-             else if (resultset.getString("ROOM_STATUS").equalsIgnoreCase("YELLOW")) {
-                 box.setFill(Color.YELLOW);
-             }
-             else{
-                 box.setFill(Color.GREEN);
-             }
-             String name = resultset.getString("ROOM_ID"); // Replace with the actual name you want to display
-             Text nameText = new Text(name);
-             nameText.setId(Integer.toString((row-1)*4+col));
-             nameText.setWrappingWidth(100); // Set the width of the text box
-             nameText.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-             nameText.setTextAlignment(TextAlignment.CENTER);
-             nameText.setMouseTransparent(true);
-
-             box.setStroke(Color.BLACK);
-             box.setStrokeWidth(2);
-
-             box.setOnMouseClicked(event -> {
-                 try {
-                     room_info(box.getId());
-                 } catch (IOException e) {
-                     throw new RuntimeException(e);
-                 } catch (SQLException e) {
-                     throw new RuntimeException(e);
-                 }
-             });
-
-             gridPane.add(box, col, row+1); // Add the box to the GridPane
-             gridPane.add(nameText, col, row + 1);
-             col = (col%4) + 1;
-         }
-     }
-*/
 
     }
 }
+
 
 
 
