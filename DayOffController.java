3d1c5627a151a0a257e619705c3cc3d9cb14ed8a
vpanagiotou531 <126961043+vpanagiotou531@users.noreply.github.com>
2023-06-11 23:10:32 +0300

    Add files via upload

--- DayOffController.java
+++ DayOffController.java
@@ -1,41 +1,125 @@
 package com.example.software_technology;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 import java.sql.*;
 
-public class DayOffController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/hoteliday_db"; // Replace with your database URL
-        String username = "root"; // Replace with your database username
-        String password = "root"; // Replace with your database password
+import java.io.IOException;
 
-        String query = "SELECT * FROM ADMIN"; // Replace with your table name
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DayOffController implements Initializable {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/hoteliday_db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+
+
+    private Stage primary_stage = HotelMain.get_stage();
+    private Connection connection = HotelMain.get_connection();
+
+
+    @FXML
+    private ChoiceBox<String> employees;
+
+    @FXML
+    void getEmployee(MouseEvent event) {
+        String employee = employees.getSelectionModel().getSelectedItem();
+        System.out.println(employee);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        employees.getItems().addAll("Matthew Roberts" , "Ava Turner", "William Cooper", "Chloe Garcia", "Ethan Parker");
+    }
+
+    @FXML
+    void date_selection_from_calendar() throws IOException, SQLException{
+
+        System.out.println("Date selected");
+    }
+
+
+
+    @FXML
+    void back_button() throws IOException, SQLException{
+
+       HotelMain.change_scene(3); // ειχα βαλει αρχικα 0
+    }
+
+
+
+    @FXML
+    void day_off_submit() throws IOException, SQLException {
 
         try {
-            // Create a connection to the database
-            Connection connection = DriverManager.getConnection(url, username, password);
+            // Create a database connection
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            // Create a statement object
-            Statement statement = connection.createStatement();
+            // Prepare the SQL statement
+            String sql1 = "SELECT * FROM EMPLOYEE WHERE (DAYOFF = 1)";
+            PreparedStatement statement1 = connection.prepareStatement(sql1);
 
-            // Execute the query and get the result set
-            ResultSet resultSet = statement.executeQuery(query);
+            //String sql2 = "SELECT * FROM EMPLOYEE WHERE (DAYOFF = 0)";
+            //PreparedStatement statement2 = connection.prepareStatement(sql2);
 
-            // Iterate through the result set and print the data
-            while (resultSet.next()) {
-                // Retrieve data from each column by name or index
-                int ADMIN_ID = resultSet.getInt("ADMIN_ID");
-                String FIRST_NAME = resultSet.getString("FIRST_NAME");
-                // Retrieve other columns as needed
+            // Execute the statement
+            statement1.executeQuery();
+            //statement2.executeUpdate();
 
-                String LAST_NAME = resultSet.getString("LAST_NAME");
+            // Close resources
+            statement1.close();
+            //statement2.close();
+            connection.close();
 
-                // Process the retrieved data
-                System.out.println("ADMIN_ID: " + ADMIN_ID + ", FIRST_NAME: " + FIRST_NAME + "LAST_NAME: " + LAST_NAME);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("The employees that can get a dayoff are the following: Ava Turner, William Cooper, Ethan Parker");
+            System.out.println("If the employee that you selected is not in this list, then he/she cannot get a dayoff");
+            //System.out.println("The employees that cannot get a dayoff are the following");
+            //System.out.println((sql2));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
+
+
 }
